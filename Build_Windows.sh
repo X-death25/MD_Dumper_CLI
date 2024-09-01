ae5fda34ffb2c357326881aa0180d58e2e16cf7d
Jackobo Le Chocobo <117809035+JackoboLeChocobo@users.noreply.github.com>
2024-09-01 12:39:14 +0200

    Update Build_Windows.sh

--- Build_Windows.sh
+++ Build_Windows.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # sudo apt-get install mingw-w64 g++-mingw-w64
 
-cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain-Ubuntu-mingw64.cmake ./
+cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE=Toolchain-Ubuntu-mingw64.cmake ./
 make
+mkdir Windows_Build
+cp *.csv ./Windows_Build/
+cp MD_Dumper ./Windows_Build/
+cp -dR images ./Windows_Build/
 rm -rf CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
-
-
