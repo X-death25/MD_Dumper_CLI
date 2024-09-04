ff2a17dde56746b57b82bbf9ae3448a832d98dfe
Jackobo Le Chocobo <117809035+JackoboLeChocobo@users.noreply.github.com>
2024-09-04 12:08:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -178,6 +178,8 @@ unsigned char chip_id=0;
 unsigned char manufacturer_id=0;
 const char * wheel[] = { "-","\\","|","/"}; //erase wheel
 
+//MD Dumper Functions
+#include "md_dumper_read.h"
 
 void cb1(void *s, size_t len, void *data)
 {
@@ -1174,44 +1176,7 @@ int main(int argc, char *argv[])
 
     if ( dump_mode==0 && opts_choice==0 )										//Mode Automatique
     {
-        SDL_Log("\n");
-        SDL_Log("Read Mode : Read ROM in automatic mode\n");
-
-        SDL_Log("Sending command Dump ROM \n");
-        SDL_Log("Dumping please wait ...\n");
-        timer_start();
-        address=0;
-        game_size *= 1024;		//game_size=4096*1024;
-        SDL_Log("\n");
-        SDL_Log("Rom Size : %ld Ko \n",game_size/1024);
-        BufferROM = (unsigned char*)malloc(game_size);
-        // Cleaning ROM Buffer
-        for (i=0; i<game_size; i++)
-        {
-            BufferROM[i]=0x00;
-        }
-
-        usb_buffer_out[0] = READ_MD;
-        usb_buffer_out[1]=address & 0xFF;
-        usb_buffer_out[2]=(address & 0xFF00)>>8;
-        usb_buffer_out[3]=(address & 0xFF0000)>>16;
-        usb_buffer_out[4]=1;
-
-        libusb_bulk_transfer(handle, 0x01,usb_buffer_out, sizeof(usb_buffer_out), &numBytes, 0);
-        SDL_Log("ROM dump in progress...\n");
-        res = libusb_bulk_transfer(handle, 0x82,BufferROM,game_size, &numBytes, 0);
-        if (res != 0)
-        {
-            SDL_Log("Error \n");
-            return 1;
-        }
-        SDL_Log("\n");
-        SDL_Log("Dump ROM completed !\n");
-        timer_end();
-        timer_show();
-        myfile = fopen("dump_smd.bin","wb");
-        fwrite(BufferROM, 1,game_size, myfile);
-        fclose(myfile);
+        return Read_ROM_Auto();
     }
     else if ( dump_mode==1 && opts_choice==0 )								//Mode Manuel
     {
