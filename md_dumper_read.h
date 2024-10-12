ab2e4f4a1a6bdb87d034beed0eac90855381ae2e
Jackobo Le Chocobo <imbert.jacquesolivier@gmail.com>
2024-10-12 15:19:09 +0200

    Lock-On Test

--- md_dumper_read.h
+++ md_dumper_read.h
@@ -6,39 +6,81 @@ int Read_ROM_Auto(void)
         SDL_Log("Sending command Dump ROM \n");
         SDL_Log("Dumping please wait ...\n");
         timer_start();
-        address=0;
-        game_size *= 1024;		//game_size=4096*1024;
-        SDL_Log("\n");
-        SDL_Log("Rom Size : %ld Ko \n",game_size/1024);
-        BufferROM = (unsigned char*)malloc(game_size);
-        // Cleaning ROM Buffer
-        for (i=0; i<game_size; i++)
-        {
-            BufferROM[i]=0x00;
-        }
+        
+        if(csv_mapper_number==3)
+			{
+			SDL_Log("\n");
+			SDL_Log("Extra Hardware : Sonic and Knuckles Lock-On\n");
+			
+			address=(0x200100)/2;
+            usb_buffer_out[0] = READ_MD;
+            usb_buffer_out[1] = address&0xFF ;
+            usb_buffer_out[2] = (address&0xFF00)>>8;
+            usb_buffer_out[3]=(address & 0xFF0000)>>16;
+            usb_buffer_out[4] = 0; // Slow Mode
+			
+			libusb_bulk_transfer(handle, 0x01,usb_buffer_out, sizeof(usb_buffer_out), &numBytes, 60000);
+            libusb_bulk_transfer(handle, 0x82,usb_buffer_in,64, &numBytes, 60000);
+            
+            memcpy((unsigned char *)dump_name, (unsigned char *)usb_buffer_in+32,32);
+            trim((unsigned char *)dump_name, 0);
+            if(memcmp((unsigned char *)dump_name,"SONIC THE               HEDGEHOG",32) == 0)
+				{
+				game_size=2560*1024;
+				}
+            else if(memcmp((unsigned char *)dump_name,"                                ",32) == 0) // Tanglewood use fake header
+				{
+				game_size=4096*1024;
+				}
+			else if(memcmp((unsigned char *)dump_name,"SONIC THE             HEDGEHOG 2",32) == 0)
+				{
+				game_size=3328*1024;
+				}
+			else if(memcmp((unsigned char *)dump_name,"SONIC THE             HEDGEHOG 3",32) == 0)
+				{
+				game_size=4096*1024;
+				}
+			else
+				{
+				game_size=2048*1024;
+				}
+			}
+		else
+			{
+			game_size *= 1024;		//game_size=4096*1024;
+			}
+		
+		SDL_Log("\n");
+		SDL_Log("Rom Size : %ld Ko \n",game_size/1024);
+		BufferROM = (unsigned char*)malloc(game_size);
+		// Cleaning ROM Buffer
+		for (i=0; i<game_size; i++)
+		{
+			BufferROM[i]=0x00;
+		}
+		address=0;
+		usb_buffer_out[0] = READ_MD;
+		usb_buffer_out[1]=address & 0xFF;
+		usb_buffer_out[2]=(address & 0xFF00)>>8;
+		usb_buffer_out[3]=(address & 0xFF0000)>>16;
+		usb_buffer_out[4]=1;
 
-        usb_buffer_out[0] = READ_MD;
-        usb_buffer_out[1]=address & 0xFF;
-        usb_buffer_out[2]=(address & 0xFF00)>>8;
-        usb_buffer_out[3]=(address & 0xFF0000)>>16;
-        usb_buffer_out[4]=1;
-
-        libusb_bulk_transfer(handle, 0x01,usb_buffer_out, sizeof(usb_buffer_out), &numBytes, 0);
-        SDL_Log("ROM dump in progress...\n");
-        res = libusb_bulk_transfer(handle, 0x82,BufferROM,game_size, &numBytes, 0);
-        if (res != 0)
-        {
-            SDL_Log("Error \n");
-            return 1;
-        }
-        SDL_Log("\n");
-        SDL_Log("Dump ROM completed !\n");
-        timer_end();
-        timer_show();
-        myfile = fopen("dump_smd.bin","wb");
-        fwrite(BufferROM, 1,game_size, myfile);
-        fclose(myfile);
-        return 0;
+		libusb_bulk_transfer(handle, 0x01,usb_buffer_out, sizeof(usb_buffer_out), &numBytes, 0);
+		SDL_Log("ROM dump in progress...\n");
+		res = libusb_bulk_transfer(handle, 0x82,BufferROM,game_size, &numBytes, 0);
+		if (res != 0)
+		{
+			SDL_Log("Error \n");
+			return 1;
+		}
+		SDL_Log("\n");
+		SDL_Log("Dump ROM completed !\n");
+		timer_end();
+		timer_show();
+		myfile = fopen("dump_smd.bin","wb");
+		fwrite(BufferROM, 1,game_size, myfile);
+		fclose(myfile);
+		return 0;
 }
 
 int Read_ROM_Manual(void)
